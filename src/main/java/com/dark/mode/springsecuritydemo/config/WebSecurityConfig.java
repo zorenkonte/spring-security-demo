01e00b5c5a6735261b2835ce633bf95cc75b498f
DarkMode : On <muffintoppings36@gmail.com>
2020-12-05 21:53:31 +0800

    initial commit

---
+++ src/main/java/com/dark/mode/springsecuritydemo/config/WebSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.dark.mode.springsecuritydemo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("zoren").password("{noop}letmein").roles("USER")
+                .and()
+                .withUser("darkmode").password("{noop}darkroot").roles("USER");
+    }
+
+}
---
+++ src/main/java/com/dark/mode/springsecuritydemo/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.dark.mode.springsecuritydemo.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home(Authentication a, Model m) {
+        m.addAttribute("user", a.getName());
+        return "index";
+    }
+
+}
