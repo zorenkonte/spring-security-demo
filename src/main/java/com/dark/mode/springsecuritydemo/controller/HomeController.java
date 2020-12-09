2948a9cbf0b7ec69139f0071eafee44e83894173
DarkMode : On <muffintoppings36@gmail.com>
2020-12-09 18:46:10 +0800

    show logged in user info

--- src/main/java/com/dark/mode/springsecuritydemo/controller/HomeController.java
+++ src/main/java/com/dark/mode/springsecuritydemo/controller/HomeController.java
@@ -1,16 +1,13 @@
 package com.dark.mode.springsecuritydemo.controller;
 
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("/")
-    public String home(Authentication a, Model m) {
-        m.addAttribute("user", a.getName());
+    public String home() {
         return "index";
     }
 }
