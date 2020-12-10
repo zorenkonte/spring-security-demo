ea34ed4f740a4e11f448b58c48458f5ea8235b7f
DarkMode : On <muffintoppings36@gmail.com>
2020-12-10 16:50:43 +0800

    password encode

--- src/main/java/com/dark/mode/springsecuritydemo/config/WebSecurityConfig.java
+++ src/main/java/com/dark/mode/springsecuritydemo/config/WebSecurityConfig.java
@@ -1,11 +1,14 @@
 package com.dark.mode.springsecuritydemo.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -15,11 +18,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
                 .inMemoryAuthentication()
-                .withUser("zoren").password("{noop}letmein").roles("EMPLOYEE")
+                .withUser("zoren").password(encode("letmein")).roles("EMPLOYEE")
                 .and()
-                .withUser("darkmode").password("{noop}darkroot").roles("EMPLOYEE", "MANAGER")
+                .withUser("darkmode").password(encode("darkroot")).roles("EMPLOYEE", "MANAGER")
                 .and()
-                .withUser("root").password("{noop}admin").roles("EMPLOYEE", "ADMIN");
+                .withUser("root").password(encode("admin")).roles("EMPLOYEE", "ADMIN")
+                .and()
+                .withUser("super").password(encode("sudo")).roles("EMPLOYEE", "ADMIN", "MANAGER");
+    }
+
+    private String encode(String p) {
+        return passwordEncoder().encode(p);
     }
 
     @Override
@@ -49,9 +58,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    public void configure(WebSecurity web) throws Exception {
+    public void configure(WebSecurity web) {
         web
                 .ignoring()
                 .antMatchers("/webjars/**");
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
