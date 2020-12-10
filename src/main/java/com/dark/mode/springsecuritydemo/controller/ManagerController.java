7add1965451f578b06426b112263d95ba055a42b
DarkMode : On <muffintoppings36@gmail.com>
2020-12-10 13:58:27 +0800

    admin and manager controller

---
+++ src/main/java/com/dark/mode/springsecuritydemo/controller/ManagerController.java
@@ -0,0 +1,13 @@
+package com.dark.mode.springsecuritydemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ManagerController {
+
+    @RequestMapping("/manager")
+    public String manager() {
+        return "manager";
+    }
+}
