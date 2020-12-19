9b17d61993881cd20526eff3f3aa08bee341ebaf
Dark Mode : On <bringino.renzo36@gmail.com>
2020-12-19 11:46:04 +0800

    user model

---
+++ src/main/java/com/dark/mode/springsecuritydemo/model/User.java
@@ -0,0 +1,50 @@
+package com.dark.mode.springsecuritydemo.model;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String username;
+    private String password;
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_authority",
+            joinColumns = {@JoinColumn(name = "user_id")},
+            inverseJoinColumns = {@JoinColumn(name = "authority_id")}
+    )
+    private Set<Authority> authorities = new HashSet<>();
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Authority> getAuthorities() {
+        return authorities;
+    }
+
+    public void setAuthorities(Set<Authority> authorities) {
+        this.authorities = authorities;
+    }
+}
--- src/main/java/com/dark/mode/springsecuritydemo/service/CustomerService.java
+++ src/main/java/com/dark/mode/springsecuritydemo/service/CustomerService.java
@@ -1,9 +1,7 @@
 package com.dark.mode.springsecuritydemo.service;
 
 import com.dark.mode.springsecuritydemo.model.Customer;
-import org.springframework.stereotype.Service;
 
-@Service
 public interface CustomerService {
     Iterable<Customer> customers();
 }
