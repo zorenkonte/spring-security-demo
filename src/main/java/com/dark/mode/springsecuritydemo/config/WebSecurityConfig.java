be553d774e3e23e249427981a714f15b19308c9f
Dark Mode : On <bringino.renzo36@gmail.com>
2020-12-20 12:36:48 +0800

    set password encoder to bcrypt

--- src/main/java/com/dark/mode/springsecuritydemo/config/WebSecurityConfig.java
+++ src/main/java/com/dark/mode/springsecuritydemo/config/WebSecurityConfig.java
@@ -11,7 +11,7 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
@@ -66,7 +66,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
