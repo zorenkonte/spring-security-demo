ca28b5a478e81bd951c6bc40fe965bf11f02972c
Dark Mode : On <bringino.renzo36@gmail.com>
2020-12-20 12:17:43 +0800

    user dao as authentication provider

--- src/main/java/com/dark/mode/springsecuritydemo/config/WebSecurityConfig.java
+++ src/main/java/com/dark/mode/springsecuritydemo/config/WebSecurityConfig.java
@@ -1,34 +1,34 @@
 package com.dark.mode.springsecuritydemo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final UserDetailsService userDetailsService;
+
+    @Autowired
+    public WebSecurityConfig(@Qualifier("customService") UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
-                .inMemoryAuthentication()
-                .withUser("zoren").password(encode("letmein")).roles("EMPLOYEE")
-                .and()
-                .withUser("darkmode").password(encode("darkroot")).roles("EMPLOYEE", "MANAGER")
-                .and()
-                .withUser("root").password(encode("admin")).roles("EMPLOYEE", "ADMIN")
-                .and()
-                .withUser("super").password(encode("sudo")).roles("EMPLOYEE", "ADMIN", "MANAGER");
-    }
-
-    private String encode(String p) {
-        return passwordEncoder().encode(p);
+                .authenticationProvider(authenticationProvider());
     }
 
     @Override
@@ -66,6 +66,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return NoOpPasswordEncoder.getInstance();
     }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
 }
